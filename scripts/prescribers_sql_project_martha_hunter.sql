93687a8fd4a9e0f191f84def55a341f4f59afef5
marthahunter <mgoodemotehunter@gmail.com>
2023-10-10 21:28:37 -0400

    working on 1b

--- scripts/prescribers_sql_project_martha_hunter.sql
+++ scripts/prescribers_sql_project_martha_hunter.sql
@@ -1,5 +1,12 @@
 -- 1a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
 
+SELECT
+	npi,
+	(COUNT(total_claim_count)) AS number_of_claims
+FROM prescription
+GROUP BY npi
+ORDER BY number_of_claims DESC;
+
 -- 1b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name, specialty_description, and the total number of claims.
 
 
